41409deac44bc4b90effcfaf25282c7dd1f05633
Ajaysdhillon <ajaysdhillon@gmail.com>
2022-08-04 17:41:46 +0530

    Creating our sidebar component and adding images to it

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,17 @@
+import './index.scss'
+import React from 'react'
+import { Link } from 'react-router-dom'
+import LogoS from '../../assets/images/logo-s.png'
+import LogoSubtitle from '../../assets/images/logo_sub.png'
+const Sidebar = () => {
+  return (
+    <div className="nav-bar">
+      <Link className="logo" to="/">
+        <img src={LogoS} alt="logo" />
+        <img src={LogoSubtitle} alt="" />
+      </Link>
+    </div>
+  )
+}
+
+export default Sidebar
