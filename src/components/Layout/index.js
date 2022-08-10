8b2c0a8b697ca53cc78794f1e3e58bccb3921451
Ajaysdhillon <ajaysdhillon@gmail.com>
2022-08-10 16:57:26 +0530

    Adding elemets to index.js for creation of site layout

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -4,8 +4,17 @@ import Sidebar from '../Sidebar'
 
 const Layout = () => {
   return (
-    <div>
+    <div className="App">
       <Sidebar />
+      <div className="page">
+        <span className="tags top-tags">&lt;body&gt;</span>
+        <Outlet />
+        <span className="tags bottom-tags">
+          &lt;/body&gt;
+          <br />
+          <span className="bottom-tag-html">&lt;/html&gt;</span>
+        </span>
+      </div>
     </div>
   )
 }
