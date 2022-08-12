6fb8a6f452f9f93c814ea846c36cbee08dcef995
Ajaysdhillon <ajaysdhillon@gmail.com>
2022-08-12 16:55:15 +0530

    Creating an animated letter component to animate the characters written statically to behave in a certain way

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,16 +1,47 @@
 import LogoTitle from '../../assets/images/logo-s.png'
 import { Link } from 'react-router-dom'
 import './index.scss'
+import { useState } from 'react'
+import AnimatedLetters from '../AnimatedLetters'
 const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  const nameArray = ['l', 'o', 'b', 'o', 'd', 'a', 'n']
+  const jobArray = [
+    'w',
+    'e',
+    'b',
+    ' ',
+    'd',
+    'e',
+    'v',
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r',
+    '.',
+  ]
+
   return (
     <div className="container home-page">
       <div className="text-zone">
         <h1>
-          Hi, <br /> I'm
+          <span className={letterClass}>H</span>
+          <span className={`{letterClass} _12`}>i,</span>
           <img src={LogoTitle} alt="developer" />
-          jay Singh
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={nameArray}
+            idx={15}
+          />
           <br />
-          Web Developer
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={jobArray}
+            idx={22}
+          />
         </h1>
         <h2>Frontend Developer</h2>
         <Link to="/contact" className="flat-button">
