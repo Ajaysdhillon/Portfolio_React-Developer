6b38970db5514bf4fad54491180de7af9e8ba2a5
Ajaysdhillon <ajaysdhillon@gmail.com>
2022-08-12 16:26:09 +0530

    Adding information to home component and styles to index.scss

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,13 +1,21 @@
 import LogoTitle from '../../assets/images/logo-s.png'
-
+import { Link } from 'react-router-dom'
+import './index.scss'
 const Home = () => {
   return (
     <div className="container home-page">
       <div className="text-zone">
         <h1>
-          Hi, <br /> I'm'
+          Hi, <br /> I'm
+          <img src={LogoTitle} alt="developer" />
+          Ajay Singh Dhillon
+          <br />
+          Web Developer
         </h1>
-        <img src={LogoTitle} alt=""></img>
+        <h2>Frontend Developer</h2>
+        <Link to="/contact" className="flat-button">
+          CONTACT ME
+        </Link>
       </div>
     </div>
   )
