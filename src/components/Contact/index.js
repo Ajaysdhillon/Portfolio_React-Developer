a9e77074bc091c1038c68b1d4afe572c20b5ab49
Ajaysdhillon <ajaysdhillon@gmail.com>
2022-08-21 17:45:18 +0530

    Adding some default styles and animation in the contact page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,7 +1,32 @@
+import Loader from 'react-loaders'
 import './index.scss'
+import { useEffect, useState } from 'react'
+
+import AnimatedLetters from '../AnimatedLetters'
 
 const Contact = () => {
-  'Hello'
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+  }, [])
+  return (
+    <>
+      <div className="container contact-page"></div>
+      <Loader type="pacman" />
+      <div className="text-zone">
+        <h1>
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']}
+            idx={15}
+          />
+        </h1>
+      </div>
+    </>
+  )
 }
 
 export default Contact
